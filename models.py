9ecb41e5f0ec3c3b098ffdd7fd83dde21c18d34a
matt <matt17@qq.com>
2021-03-29 13:44:29 +0800

    增加笔记分享

--- models.py
+++ models.py
@@ -86,3 +86,95 @@ class Note(db.Model):
             'updated_at': self.updated_at.strftime('%Y/%m/%d %H:%M:%S'),
             'user_id': self.user_id,
         }
+
+    @staticmethod
+    def get_note_by_id(id):
+        note = Note.query.filter_by(id=id).first()
+        return note
+
+
+class NoteForward(db.Model):
+    """ 笔记分享 """
+    __tablename__ = 'note_note_forward'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 主键
+    content = db.Column(db.Text)
+    is_valid = db.Column(db.SmallInteger, default=1)
+    view_count = db.Column(db.String(256), default=1)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改的时间
+    updated_at = db.Column(db.DateTime,
+                           default=datetime.now, onupdate=datetime.now)
+    # 关联用户
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    note_id = db.Column(db.Integer, db.ForeignKey('note_note.id'))
+    # 建立用户的一对一关系属性user.note_list  note.user
+    user = db.relationship('User', backref=db.backref('note_forward_list', lazy='dynamic'))
+    note = db.relationship('Note', backref=db.backref('note_forward_list', lazy='dynamic'))
+
+    """序列化"""
+    def serialize(self):
+        return {
+            'id': self.id,
+            'view_count': self.view_count,
+            'content': self.content,
+            'is_valid': self.is_valid,
+            'created_at': self.created_at.strftime('%Y/%m/%d %H:%M:%S'),
+            'updated_at': self.updated_at.strftime('%Y/%m/%d %H:%M:%S'),
+            'user_id': self.user_id,
+            'note_id': self.note_id
+        }
+
+
+
+class NoteTag(db.Model):
+    """ 笔记分享 """
+    __tablename__ = 'note_note_tag'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 主键
+    tag_name = db.Column(db.String(128))
+    is_valid = db.Column(db.SmallInteger, default=1)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改的时间
+    updated_at = db.Column(db.DateTime,
+                           default=datetime.now, onupdate=datetime.now)
+    # 关联用户
+    note_id = db.Column(db.Integer, db.ForeignKey('note_note.id'))
+    # 建立用户的一对一关系属性user.note_list  note.user
+    note = db.relationship('Note', backref=db.backref('note_tag_list', lazy='dynamic'))
+
+    """序列化"""
+    def serialize(self):
+        return {
+            'id': self.id,
+            'tag_name': self.tag_name,
+            'is_valid': self.is_valid,
+            'created_at': self.created_at.strftime('%Y/%m/%d %H:%M:%S'),
+            'updated_at': self.updated_at.strftime('%Y/%m/%d %H:%M:%S'),
+            'note_id': self.note_id
+        }
+
+
+class NoteCollect(db.Model):
+    """ 笔记分享 """
+    __tablename__ = 'note_note_collect'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 主键
+    is_valid = db.Column(db.SmallInteger, default=1)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 关联用户
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    note_id = db.Column(db.Integer, db.ForeignKey('note_note.id'))
+    # 建立用户的一对一关系属性user.note_list  note.user
+    user = db.relationship('User', backref=db.backref('note_collect_list', lazy='dynamic'))
+    note = db.relationship('Note', backref=db.backref('note_collect_list', lazy='dynamic'))
+
+    """序列化"""
+    def serialize(self):
+        return {
+            'id': self.id,
+            'is_valid': self.is_valid,
+            'created_at': self.created_at.strftime('%Y/%m/%d %H:%M:%S'),
+            'user_id': self.user_id,
+            'note_id': self.note_id
+        }
--- note/views.py
+++ note/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, make_response, jsonify
 from flask_cors import cross_origin
 from app import db
-from models import Note
+from models import Note, NoteForward
 
 note = Blueprint('note', __name__)
 
@@ -11,7 +11,7 @@ note = Blueprint('note', __name__)
 def save_note():
     param_note = request.get_json(silent=True)
 
-    if param_note.get('id') != -1:
+    if param_note.get('id') != None:
         print('hello word')
         # 更新操作
         update_note = Note.query.filter_by(id=param_note.get('id')).first()
@@ -32,17 +32,57 @@ def save_note():
     return make_response('success', 200)
 
 
+@note.route('/remove/<id>', methods=['GET'])
+@cross_origin(origins='*', supports_credentials=True)
+def remove_note_by_id(id):
+    remove_note = Note.query.filter_by(id=id).first()
+    remove_note.is_valid = 0
+    db.session.add(remove_note)
+    db.session.commit()
+    note_list = Note.query.filter_by(user_id=remove_note.user_id, is_valid=1).all()
+    return jsonify(res_note_list=[i.serialize() for i in note_list])
+
+
 @note.route('/list', methods=['GET'])
 @cross_origin(origins='*', supports_credentials=True)
 def list_note():
     param_user_id = request.args.get('uid')
-    note_list = Note.query.filter_by(user_id=param_user_id).all()
-    return jsonify(res_note_list = [i.serialize() for i in note_list])
+    note_list = Note.query.filter_by(user_id=param_user_id, is_valid=1).all()
+    return jsonify(res_note_list=[i.serialize() for i in note_list])
 
 
 @note.route('/<id>', methods=['GET'])
 @cross_origin(origins='*', supports_credentials=True)
 def get_note_by_id(id):
-
     note = Note.query.filter_by(id=id).first()
     return jsonify(note.serialize())
+
+
+@note.route('/forward/save', methods=['POST'])
+@cross_origin(origins='*', supports_credentials=True)
+def forward_note_by_id():
+    param_data = request.get_json(silent=True)
+    note_forward = NoteForward()
+    note_forward.user_id = param_data.get('userId')
+    note_forward.note_id = param_data.get('noteId')
+    note_forward.content = param_data.get('forwardContent')
+    db.session.add(note_forward)
+    db.session.commit()
+    print(param_data)
+    return 'success'
+
+
+@note.route('/forward/list', methods=['GET'])
+@cross_origin(origins='*', supports_credentials=True)
+def list_note_forward():
+    db_note_forward_list = NoteForward.query.filter_by(is_valid=1).all()
+    print(db_note_forward_list)
+    note_forward_list = []
+    for note_forward in db_note_forward_list:
+        note_forward = note_forward.serialize()
+        note = Note.get_note_by_id(note_forward.get('note_id'))
+        note_forward.setdefault('title', note.title)
+        note_forward_list.append(note_forward)
+
+    print(note_forward_list)
+    return jsonify(res_note_forward_list=[i for i in note_forward_list])
